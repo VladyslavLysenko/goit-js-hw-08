c9bf7e447026cdec0387b6899bb778ff0a65ee77
VladyslavLysenko <Dodog12362@gmail.com>
2022-10-02 14:51:52 +0300

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -27,20 +27,7 @@ const savedTimeVideoParse = JSON.parse(savedTimeVideo);
 console.log(savedTimeVideoParse);
 
 if (savedTimeVideoParse !== null) {
-   player.setCurrentTime(savedTimeVideoParse).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
-});
- 
+   player.setCurrentTime(savedTimeVideoParse)
 }
 
 
